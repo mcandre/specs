36c7d914eb85ddd229065bd87b7fa6fc75b37f40
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-10-23 21:26:45 -0400

    latex

--- aspects/latex.rb
+++ aspects/latex.rb
@@ -1,19 +1,7 @@
 module Recipe
-	def Recipe.mactex
-		if Os.mac?
-			"which pdflatex"
-		end
-	end
-
-	def Recipe.pdflatex
+	def Recipe.latex
 		if Os.unix?
 			"pdflatex --version"
-		end
-	end
-
-	def Recipe.latex
-		if Os.mac?
-			[mactex, pdflatex]
 		end
 	end
 end
