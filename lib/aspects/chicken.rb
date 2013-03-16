7ca56977d82ca7d50eeb3b46d6acdfdf75660416
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-03-16 17:01:21 -0400

    closes #7

---
+++ lib/aspects/chicken.rb
@@ -0,0 +1,17 @@
+module Recipe
+  def self.eggs
+    "chicken-install -version"
+  end
+
+  def self.csi
+    "csi -version"
+  end
+
+  def self.csc
+    "csc -version"
+  end
+
+  def self.chicken
+    [eggs, csi]
+  end
+end
