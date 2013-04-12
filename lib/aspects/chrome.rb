90c16dd939bba75f8dd1e2aa6e25ec6f522c3696
mcandre <andrew.pennebaker@gmail.com>
2013-04-12 07:54:21 -0400

    improved windows xp support for 'chrome'

--- lib/aspects/chrome.rb
+++ lib/aspects/chrome.rb
@@ -4,11 +4,7 @@ module Recipe
     if Os.mac?
       "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --version 2>&1 | grep -v Unsure" # Redirect stderr to stdout
     elsif Os.windows?
-      if Os.x86_64?
-        "%UserProfile%\\AppData\\Local\\Google\\Chrome\\Application\\chrome --version"
-      else
-        "\"C:\\Program Files\\Google\\Chrome\\Application\\chrome\" --version"
-      end
+      "reg query \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Google\\Update\\Clients\\{4DC8B4CA-1BDA-483e-B5FA-D3C12E15B62D}\""
       # Assumes
       # * binary is google-chrome.
       # * binary is in PATH.
