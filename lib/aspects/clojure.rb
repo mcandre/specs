a8ab7e432184f58565491a065e4e922e8506d6d7
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-06-15 01:12:17 -0400

    fixed clojure

--- lib/aspects/clojure.rb
+++ lib/aspects/clojure.rb
@@ -4,7 +4,7 @@ module Recipe
   end
 
   def self.clj
-    "lein exec '(println \"Clojure \" (clojure-version))'"
+    "lein exec -e '(println \"Clojure \" (clojure-version))'"
   end
 
   def self.clojure
