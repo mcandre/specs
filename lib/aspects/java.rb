e9bf919c4fa0fadd564d2bcff5bf5af5bc30b190
Andrew Pennebaker <apennebaker@42six.com>
2013-08-27 10:03:32 -0400

    CLASSPATH and JAVA_HOME

--- lib/aspects/java.rb
+++ lib/aspects/java.rb
@@ -3,11 +3,27 @@ module Recipe
     "mvn --version"
   end
 
+  def self.classpath
+    if Os.windows?
+      "echo %CLASSPATH"
+    else
+      "echo $CLASSPATH"
+    end
+  end
+
+  def self.javahome
+    if Os.windows?
+      "echo %JAVA_HOME%"
+    else
+      "echo $JAVA_HOME"
+    end
+  end
+
   def self.oak
     "java -version"
   end
 
   def self.java
-    [mvn, oak]
+    [mvn, classpath, javahome, oak]
   end
 end
