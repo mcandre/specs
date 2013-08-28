fee37df7d94703655dc59895fdf1248387dc820f
Andrew Pennebaker <apennebaker@42six.com>
2013-08-28 08:06:59 -0400

    move away from deprecated hadoop variables

--- lib/aspects/hadoop.rb
+++ lib/aspects/hadoop.rb
@@ -1,5 +1,17 @@
 module Recipe
-  def self.hadoop
+  def self.hadoophome
+    if Os.windows?
+      "echo %HADOOP_PREFIX%"
+    else
+      "echo $HADOOP_PREFIX"
+    end
+  end
+
+  def self.hadoopv
     "hadoop version"
+  end
+
+  def self.hadoop
+    [hadoophome, hadoopv]
   end
 end
