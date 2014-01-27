e0bb9a791c9c7125cbfc05624cdc84f086777d5f
Andrew Pennebaker <apennebaker@42six.com>
2014-01-27 13:12:18 -0500

    windows bat

--- bin/specs.bat
+++ bin/specs.bat
@@ -3,4 +3,4 @@ IF NOT "%~f0" == "~f0" GOTO :WinNT
 ECHO.This version of Ruby has not been built with support for Windows 95/98/Me.
 GOTO :EOF
 :WinNT
-@"%~dp0ruby.exe" "%~dpn0" %*
+@ruby "%~dpn0" %*
