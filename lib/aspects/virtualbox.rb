7cf87ac2e43967155b8b3d3605fed9b390465d05
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-02-03 16:24:16 -0500

    fixed virtualbox (windows)

--- lib/aspects/virtualbox.rb
+++ lib/aspects/virtualbox.rb
@@ -2,10 +2,10 @@ module Recipe
   def self.virtualbox
     case Os.os_name
     when :windows
-      if Os.x86?
-        '"C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage" --version'
+      if Os.x86_64?
+        '"C:\\Program Files\\Oracle\\VirtualBox\\VBoxManage.exe" --version'
       else
-        'C:\\Program Files (x86)\\Oracle\\VirtualBox\\VBoxManage --version'
+        '"C:\\Program Files (x86)\\Oracle\\VirtualBox\\VBoxManage.exe" --version'
       end
     when :mac
       'vboxwebsrv --help 2>&1 | grep VirtualBox'
