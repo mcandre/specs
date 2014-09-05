5ded7bae70a14ac12645bae98f982e8448f6758c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-09-04 21:46:28 -0500

    grep efficiency

--- lib/aspects/terminal.rb
+++ lib/aspects/terminal.rb
@@ -1,7 +1,7 @@
 module Recipe
   def self.terminal
     if Os.mac?
-      'system_profiler -detailLevel full 2>&1 | grep Terminal -A 5 | grep Version | head -n 1'
+      'system_profiler -detailLevel full 2>&1 | grep Terminal -A 5 | grep Version -m 1'
     end
   end
 end
