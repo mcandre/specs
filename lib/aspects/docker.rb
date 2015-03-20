2f14ea53f4ffb30146d996c41903fbc04b857872
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-03-20 17:29:32 -0500

    docker

---
+++ lib/aspects/docker.rb
@@ -0,0 +1,17 @@
+module Recipe
+  def self.dockerv
+    'docker version'
+  end
+
+  def self.boot2docker
+    'boot2docker version'
+  end
+
+  def self.docker
+    if Os.linux?
+      [dockerv]
+    else
+      [dockerv, boot2docker]
+    end
+  end
+end
